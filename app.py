ebb32cbe49ce2a2998c43682d91ea368c78c2e3b
Jobcheruyot <jobcheruyot@gmail.com>
2025-11-11 10:06:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,19 @@ import plotly.graph_objects as go
 from datetime import timedelta
 
 st.set_page_config(layout="wide", page_title="Superdeck (Streamlit)")
+# Add the following at the very top of your Streamlit script, AFTER st.set_page_config
+
+import streamlit as st
+
+# Hide Streamlit default header and footer
+hide_st_style = """
+    <style>
+    header {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+"""
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 
 # -----------------------
 # Data Loading & Caching
