f09cd3a0d1b9eb5e8572d2988c67d28dea43f405
Jobcheruyot <jobcheruyot@gmail.com>
2026-02-04 15:41:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ from datetime import timedelta
 
 st.set_page_config(layout="wide", page_title="Superdeck (Streamlit)")
 # Add the following at the very top of your Streamlit script, AFTER st.set_page_config
-st.set_option("dataframe.convertArrow", False)
-
 
 
 
